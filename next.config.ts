a1798e260aeeea0fd6e07c89e66812676dee43e6
Alex-Clau <alexgafita47@gmail.com>
2025-11-26 20:35:13 +0200

    chore: created next.js project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // tells next.js to generate static html
+  images: {
+    unoptimized: true // required: S3 cannot resize images on the fly like Vercel
+  },
+  trailingSlash: true // clean up html output
 };
 
 export default nextConfig;
